c6e72c5d50c31b4605271ca80d16de5e8bee6fee
Simona Cinti <simona.cinti2@gmail.com>
2020-10-20 14:53:24 +0200

    FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("hello javascript");
+
+//todo Scrivi un programma che stampi i numeri da 1 a 100,
+//todo ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+//todo Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+
+var numeri = [];
+var somma = 0;
+
+
+for (i= 0; i < 100; i++){
+
+    if ((numeri % 3 == 0) && (numeri % 5 == 0)){
+        console.log('FizzBuzz');
+    }
+
+    else if (numeri % 3 == 0){
+        console.log('Fizz');
+    }
+
+    else if (numeri % 5 == 0){
+        console.log('Buzz');
+    }
+    
+    else {
+        console.log(numeri);
+    }
+    numeri++;
+    
+}
+
