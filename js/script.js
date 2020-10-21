214ae9b24bd7303326da38f3da47d3ebb30dd6dc
Simona Cinti <simona.cinti2@gmail.com>
2020-10-21 09:54:04 +0200

    correction

--- js/script.js
+++ js/script.js
@@ -3,30 +3,26 @@
 //todo ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
 //todo Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
 
-// Variabili
-var numeri = 1;
-var somma = 0;
-
 // Ciclo
-for (i= 0; i < 100; i++){
+for (i= 1; i <= 100; i++){
     //Numero divisibile per 3 e per 5
-    if ((numeri % 3 == 0) && (numeri % 5 == 0)){
+    if ((i % 3 == 0) && (i % 5 == 0)){
         console.log('FizzBuzz');
     }
     //Numero divisibile per 3
-    else if (numeri % 3 == 0){
+    else if (i % 3 == 0){
         console.log('Fizz');
     }
     //Numero divisibile per 5
-    else if (numeri % 5 == 0){
+    else if (i % 5 == 0){
         console.log('Buzz');
     }
     
     else {
-        console.log(numeri);
+        console.log(i);
     }
 
-    numeri++;
+
     
 }
 
